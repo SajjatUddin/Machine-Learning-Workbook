ae6aa071feee3dc23b25bc3c7d8852948612de6d
Md. Sajjad Uddin <54749005+SajjatUddin@users.noreply.github.com>
2020-04-04 23:16:59 +0600

    Update My Script.R

--- My Script.R
+++ My Script.R
@@ -164,3 +164,115 @@ table(combined_set$Child, combined_set$Survived)
 
 combined_set$Child <- factor(combined_set$Child)
 
+combined_set$Mother <- 'Not Mother'
+
+combined_set$Mother[combined_set$Sex == 'female' & 
+        combined_set$Parch > 0 & combined_set$Age > 18] <- 'Mother'
+
+
+table(combined_set$Mother, combined_set$Survived)
+
+combined_set$Mother <- factor
+
+
+# What's in a name, again?
+combined_set$Name[1]
+
+
+# Find the indexes for the tile piece of the name
+strsplit(combined_set$Name[1], split='[,.]')
+strsplit(combined_set$Name[1], split='[,.]')[[1]]
+strsplit(combined_set$Name[1], split='[,.]')[[1]][2]
+
+
+# Engineered variable: Title
+combined_set$Title <- strsplit(combined_set$Name, split='[,.]')[[1]][2]  # Won't work!
+combined_set$Title <- sapply(combined_set$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+combined_set$Title <- sub(' ', '', combined_set$Title)
+
+
+# Inspect new feature
+table(combined_set$Title)
+
+
+# Combined_set New Small Title Groups
+combined_set$Title[combined_set$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combined_set$Title[combined_set$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combined_set$Title[combined_set$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+
+
+# Convert to a factor
+combined_set$Title <- factor(combined_set$Title)
+
+
+# Adding Mother variable
+combined_set$Mother <- 'Not Mother'
+combined_set$Mother[combined_set$Sex == 'female' 
+        & combined_set$Parch > 0 & combined_set$Age > 18
+        & combined_set$Title != 'Miss'] <- 'Mother'
+
+
+# Cabin
+combined_set$Cabin[1:20]
+combined_set$Cabin <- as.character(combined_set$Cabin)
+strsplit(combined_set$Cabin[2], NULL)[[1]]
+combined_set$Deck<-factor(sapply(combined_set$Cabin, function(x) strsplit(x, NULL)[[1]][1])) 
+
+# Fare Price
+
+combined_set$Fare_type[combined_set$Fare<50]<-"low"
+combined_set$Fare_type[combined_set$Fare>50 & combined_set$Fare<=100]<-"med1"
+combined_set$Fare_type[combined_set$Fare>100 & combined_set$Fare<=150]<-"med2"
+combined_set$Fare_type[combined_set$Fare>150 & combined_set$Fare<=500]<-"high"
+combined_set$Fare_type[combined_set$Fare>500]<-"vhigh"
+
+aggregate(Survived~Fare_type, data=combined_set,mean)
+
+# Engineered variable: Family
+
+combined_set$Surname <- sapply(combined_set$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+combined_set$FamilyID <- paste(as.character(combined_set$FamilySize), combined_set$Surname, sep="")
+
+combined_set$FamilyID[combined_set$FamilySize <= 2] <- 'Small'
+
+combined_set$FamilySizeGroup[combined_set$FamilySize == 1] <- 'single'
+combined_set$FamilySizeGroup[combined_set$FamilySize < 5 & combined_set$FamilySize > 1] <- 'Smaller'
+combined_set$FamilySizeGroup[combined_set$FamilySize > 4] <- 'large'
+
+mosaicplot(table(combined_set$FamilySizeGroup, combined_set$Survived), main='Survival affected by Family Size ', shade=TRUE)
+
+
+table(combined_set$FamilyID)
+table(combined_set$FamilySizeGroup)
+
+# Convert to a factor
+
+combined_set$FamilyID <- factor(combined_set$FamilyID)
+combined_set$FamilySizeGroup <- factor(combined_set$FamilySizeGroup)
+
+# Split back into test and train sets
+
+train <- combined_set[1:891,]
+test <- combined_set[892:1309,]
+
+
+# Build a new tree with our new features
+
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + Mother + SibSp + Parch + Deck + Fare + Embarked + Title + FamilySize + FamilyID,
+             data=train, method="class")
+
+fancyRpartPlot(fit)
+
+Prediction5th <- predict(fit, test, type = "class")
+
+Prediction5 <- data.frame(PassengerId = test$PassengerId,
+                          Survived = Prediction5th)
+
+write.csv(Prediction5, file = "5thPrediction.csv", row.names = FALSE)
+
+
